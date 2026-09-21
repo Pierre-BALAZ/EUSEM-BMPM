e5790f98320e82679083c3d878d968e3c77aadce
Mediglotte <159126436+Mediglotte@users.noreply.github.com>
2026-09-21 09:46:17 +0200

    maj epass
    maj epass

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='eusem-bmpm-2026-09-20-4';
+const CACHE='eusem-bmpm-2026-09-21-1';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim()));});
